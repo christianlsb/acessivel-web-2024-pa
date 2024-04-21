2ddb8aaba8c2f51e7e14b84cb62d0b151c89eae0
christianlsb <chrislsb09@gmail.com>
2024-04-21 20:24:56 -0300

    Corrige idioma do documento para pt-br e ajusta altura do card na seção de campanhas

--- sections/campaigns.tsx
+++ sections/campaigns.tsx
@@ -17,8 +17,13 @@ const Campaigns = () => {
               {Array.from({ length: 5 }).map((_, index) => (
                 <CarouselItem key={index}>
                   <div className="p-1">
-                    <Card>
-                      <CardContent className="flex aspect-square items-center justify-center p-6 h-[370px]">
+                    <Card className="h-[370px]">
+                      <CardContent
+                        style={{
+                          height: "370px",
+                        }}
+                        className="flex aspect-square items-center justify-center p-6"
+                      >
                         <span className="text-4xl font-semibold">
                           {index + 1}
                         </span>

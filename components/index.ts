027fa32b3a648239d565c928b8d90d3f3f28b3d0
christianlsb <chrislsb09@gmail.com>
2024-06-09 16:55:33 -0300

    feat: Add CardNavigator component and update Sidebar
    The commit adds the CardNavigator component and updates the Sidebar component to use the new component. This includes importing the CardNavigator component, adding it to the Sidebar component's JSX, and updating the necessary CSS styles.

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export { default as VectorBlue } from "./vector";
 export { default as FormRegister } from "./formRegister";
 export { default as FormLogin } from "./formLogin";
 export { default as Sidebar } from "./sidebar";
+export { default as CardNavigator } from "./cardNavigator";

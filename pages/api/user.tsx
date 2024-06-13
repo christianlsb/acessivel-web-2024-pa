1d75e3689c5c5df65ea52b9fc3671baa6b81fcb7
christianlsb <chrislsb09@gmail.com>
2024-06-13 12:36:24 -0300

    feat: Add bcrypt for password hashing and validation
    This commit adds the bcrypt library for password hashing and validation. It includes updating the package.json file to add the "@types/bcrypt" and "bcrypt" dependencies. Additionally, it modifies the user.tsx API endpoint to use bcrypt for encrypting the user's password before storing it in the database.

--- pages/api/user.tsx
+++ pages/api/user.tsx
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
-
-import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,19 +10,47 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     const { nome, sobrenome, email, senha, cpf, data_nascimento } = req.body;
+
+    // verificar se o email ja existe
     try {
+      const existingUserEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (existingUserEmail) {
+        return res.status(400).json({ error: "E-mail já existe" });
+      }
+      // verificar se o cpf ja existe
+      const existingUserCpf = await prisma.user.findUnique({
+        where: {
+          cpf,
+        },
+      });
+
+      if (existingUserCpf) {
+        return res.status(400).json({ error: "CPF já existe" });
+      }
+
+      // criptografar senha
+      const hashedPassword = await bcrypt.hash(senha, 10);
+
+      // cria user
       const newUser = await prisma.user.create({
         data: {
           nome,
           sobrenome,
           email,
-          senha,
+          senha: hashedPassword,
           cpf,
           data_nascimento: new Date(data_nascimento),
         },
       });
+
       res.status(200).json(newUser);
     } catch (error) {
+      console.error("Error creating user:", error);
       res.status(500).json({ error: "Erro ao criar usuário" });
     }
   } else {

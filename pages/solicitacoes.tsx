b42580c48101ebe93570a75918c477a18a9cec05
christianlsb <chrislsb09@gmail.com>
2024-06-18 08:22:04 -0300

    feat: Add @radix-ui/react-tabs component for tabbed navigation
    The @radix-ui/react-tabs component has been added to the project to enable tabbed navigation. This component includes the Tabs, TabsList, TabsTrigger, and TabsContent components, which allow for the creation of tabbed interfaces. This enhancement improves the usability and organization of the solicitacoes page.

--- pages/solicitacoes.tsx
+++ pages/solicitacoes.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { Sidebar, Header, ContainerDashboard } from "@/components/index";
 import { GetServerSidePropsContext } from "next";
 import jwt from "jsonwebtoken";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Solicit() {
   return (
@@ -18,6 +19,58 @@ export default function Solicit() {
         <Sidebar />
         <ContainerDashboard>
           <h1>Solicitações</h1>
+          <Tabs defaultValue="account">
+            <TabsList className="flex-col">
+              <TabsTrigger value="account">Dados pessoais</TabsTrigger>
+              <TabsTrigger value="address">Endereço</TabsTrigger>
+              <TabsTrigger value="cadpcd">Vincule seu CadPCD</TabsTrigger>
+            </TabsList>
+            <TabsContent value="account">
+              <div className="max-w-[664px]">
+                <div className="flex">
+                  <div className="flex flex-col">
+                    <label>Nome</label>
+                    <input placeholder="Christian" disabled />
+                  </div>
+                  <div className="flex flex-col">
+                    <label>Nome</label>
+                    <input placeholder="Lisboa" disabled />
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="">Email</label>
+                  <input
+                    placeholder="chrislsb@gmail.com"
+                    disabled
+                    className="w-full"
+                    type="text"
+                  />
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col">
+                    <label>CPF</label>
+                    <input placeholder="000.000.000-00" disabled />
+                  </div>
+                  <div className="flex flex-col">
+                    <label>Data de nascimento</label>
+                    <input placeholder="27/07/2004" disabled />
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col">
+                    <label>Senha</label>
+                    <input placeholder="**********" disabled />
+                  </div>
+                  <div className="flex flex-col">
+                    <label>Confirmar senha</label>
+                    <input placeholder="**********" disabled />
+                  </div>
+                </div>
+              </div>
+            </TabsContent>
+            <TabsContent value="address">Endereço</TabsContent>
+            <TabsContent value="cadpcd">Vincule seu cadpcd</TabsContent>
+          </Tabs>
         </ContainerDashboard>
       </div>
     </>

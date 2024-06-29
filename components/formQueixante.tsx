96962da48fc658b07f3d5272e05b88a562795199
christianlsb <chrislsb09@gmail.com>
2024-06-29 15:25:27 -0300

    Adiciona schema para dataNascimento

--- components/formQueixante.tsx
+++ components/formQueixante.tsx
@@ -16,72 +16,79 @@ import { validate } from 'gerador-validador-cpf';
 
 /* VALIDAÇÃO DOS CAMPOS DO FORMULÁRIO */
 const formSchema = z
-  .object({
-    nome: z
-      .string()
-      .min(1, "O nome é obrigatório")
-      .max(50, "Inválido, no máximo 50 letras")
-      .regex(
-        /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u,
-        "Nome inválido"
-      )
-      .refine((campo) => campo.length >= 3, {
-        message: "Inválido, no mínimo 3 letras",
-      }),
-    sobrenome: z
-      .string()
-      .min(1, "O sobrenome é obrigatório")
-      .max(50, "Inválido, no máximo 50 letras")
-      .regex(
-        /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u,
-        "Nome inválido"
-      )
-      .refine((campo) => campo.length >= 3, {
-        message: "Inválido, no mínimo 3 letras",
-      }),
-    email: z
-      .string()
-      .min(1, "O email é obrigatório")
-      .max(100, "Inválido, no máximo 100 letras")
-      .email("Email inválido")
-      .refine((campo) => campo.length >= 5, {
-        message: "Inválido, no mínimo 5 letras",
-      }),
-    cpf: z
-        .string()
-        .min(1, "O CPF é obrigatório")
-        .length(14, "O CPF deve conter 11 números")
-        .refine((cpf) => {
-          const isValid = validate(cpf.replace(/[^\d]/g, ''));
-          return isValid;
-        }, "CPF inválido"),
-    senha: z
-      .string()
-      .min(1, "A senha é obrigatória")
-      .max(50, "Inválida, no máximo 50 carateres")
-      .regex(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{0,}$/,
-        "Senha inválida, ex:: Senha123!"
-      )
-      .refine((campo) => campo.length >= 8, {
-        message: "Inválido, no mínimo 8 caracteres",
-      }),
-    confirmPassword: z
-      .string()
-      .min(1, "A senha é obrigatória")
-      .max(50, "Inválida, no máximo 50 carateres")
-      .regex(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{0,}$/,
-        "Senha inválida, ex: Senha123!"
-      )
-      .refine((campo) => campo.length >= 8, {
-        message: "Inválido, no mínimo 8 caracteres",
-      }),
-  })
-  .refine((campos) => campos.senha === campos.confirmPassword, {
-    message: "As senhas devem ser iguais!",
-    path: ["confirmPassword"],
-  });
+    .object({
+      nome: z
+          .string()
+          .min(1, "O nome é obrigatório")
+          .max(50, "Inválido, no máximo 50 letras")
+          .regex(
+              /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u,
+              "Nome inválido"
+          )
+          .refine((campo) => campo.length >= 3, {
+            message: "Inválido, no mínimo 3 letras",
+          }),
+      sobrenome: z
+          .string()
+          .min(1, "O sobrenome é obrigatório")
+          .max(50, "Inválido, no máximo 50 letras")
+          .regex(
+              /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u,
+              "Nome inválido"
+          )
+          .refine((campo) => campo.length >= 3, {
+            message: "Inválido, no mínimo 3 letras",
+          }),
+      email: z
+          .string()
+          .min(1, "O email é obrigatório")
+          .max(100, "Inválido, no máximo 100 letras")
+          .email("Email inválido")
+          .refine((campo) => campo.length >= 5, {
+            message: "Inválido, no mínimo 5 letras",
+          }),
+      cpf: z
+          .string()
+          .min(1, "O CPF é obrigatório")
+          .length(14, "O CPF deve conter 11 números")
+          .refine((cpf) => {
+            const isValid = validate(cpf.replace(/[^\d]/g, ''));
+            return isValid;
+          }, "CPF inválido"),
+      dataNascimento: z
+          .string()
+          .min(1, "A data de nascimento é obrigatória")
+          .regex(
+              /^\d{2}\/\d{2}\/\d{4}$/,
+              "Data de nascimento inválida, use o formato DD/MM/AAAA"
+          ),
+      senha: z
+          .string()
+          .min(1, "A senha é obrigatória")
+          .max(50, "Inválida, no máximo 50 carateres")
+          .regex(
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{0,}$/,
+              "Senha inválida, ex:: Senha123!"
+          )
+          .refine((campo) => campo.length >= 8, {
+            message: "Inválido, no mínimo 8 caracteres",
+          }),
+      confirmPassword: z
+          .string()
+          .min(1, "A senha é obrigatória")
+          .max(50, "Inválida, no máximo 50 carateres")
+          .regex(
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{0,}$/,
+              "Senha inválida, ex: Senha123!"
+          )
+          .refine((campo) => campo.length >= 8, {
+            message: "Inválido, no mínimo 8 caracteres",
+          }),
+    })
+    .refine((campos) => campos.senha === campos.confirmPassword, {
+      message: "As senhas devem ser iguais!",
+      path: ["confirmPassword"],
+    });
 
 const FormQueixante = () => {
   const { toast } = useToast();
@@ -97,11 +104,10 @@ const FormQueixante = () => {
     resolver: zodResolver(formSchema),
   });
 
-  const formatDateString = (dateStr: string) => {
+  const formatDateString = (dateStr: any) => {
     const [day, month, year] = dateStr.split("/");
     return `${year}-${month}-${day}`;
   };
-
 
   const onSubmit = async (values: any) => {
     setError(null);
@@ -145,127 +151,127 @@ const FormQueixante = () => {
   };
 
   return (
-    <div className={st.container}>
-      <div className={cn(st.content)}>
-        {/* Imagem do Cachorro */}
-        <div className={st.contentImg}>
-          <Image src={dog} width={692} height={610} alt="Imagem do cachorro" />
-        </div>
-        {/* Formulário de Cadastro */}
-        <div className={st.containerForm}>
-          <div className={st.contentForm}>
-            <h1>Cadastro</h1>
-            <p>O portal que te escuta! Estamos aqui por você.</p>
-            {/* Campos */}
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className={st.fields}>
-                {/* Nome */}
-                <div className={st.field}>
-                  <label htmlFor="nome">Nome</label>
-                  <span className={st.fieldValidation}>
+      <div className={st.container}>
+        <div className={cn(st.content)}>
+          {/* Imagem do Cachorro */}
+          <div className={st.contentImg}>
+            <Image src={dog} width={692} height={610} alt="Imagem do cachorro" />
+          </div>
+          {/* Formulário de Cadastro */}
+          <div className={st.containerForm}>
+            <div className={st.contentForm}>
+              <h1>Cadastro</h1>
+              <p>O portal que te escuta! Estamos aqui por você.</p>
+              {/* Campos */}
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className={st.fields}>
+                  {/* Nome */}
+                  <div className={st.field}>
+                    <label htmlFor="nome">Nome</label>
+                    <span className={st.fieldValidation}>
                     {errors?.nome ? (errors.nome.message as string) : null}
                   </span>
-                  <input id="nome" type="text" {...register("nome")} />
-                </div>
-                {/* Sobrenome */}
-                <div className={st.field}>
-                  <label htmlFor="sobrenome">Sobrenome</label>
-                  <span className={st.fieldValidation}>
+                    <input id="nome" type="text" {...register("nome")} />
+                  </div>
+                  {/* Sobrenome */}
+                  <div className={st.field}>
+                    <label htmlFor="sobrenome">Sobrenome</label>
+                    <span className={st.fieldValidation}>
                     {errors?.sobrenome
-                      ? (errors.sobrenome.message as string)
-                      : null}
+                        ? (errors.sobrenome.message as string)
+                        : null}
                   </span>
-                  <input
-                    type="text"
-                    id="sobrenome"
-                    {...register("sobrenome")}
-                  />
+                    <input
+                        type="text"
+                        id="sobrenome"
+                        {...register("sobrenome")}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className={st.fields}>
-                {/* Email */}
-                <div className={cn(st.field, st.fieldEmail)}>
-                  <label htmlFor="email">Email</label>
-                  <span className={st.fieldValidation}>
+                <div className={st.fields}>
+                  {/* Email */}
+                  <div className={cn(st.field, st.fieldEmail)}>
+                    <label htmlFor="email">Email</label>
+                    <span className={st.fieldValidation}>
                     {errors?.email ? (errors.email.message as string) : null}
                   </span>
-                  <input
-                    id="email"
-                    className={st.email}
-                    type="text"
-                    {...register("email")}
-                  />
+                    <input
+                        id="email"
+                        className={st.email}
+                        type="text"
+                        {...register("email")}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className={st.fields}>
-                <div className={st.field}>
-                  <label htmlFor="cpf">CPF</label>
-                  <span className={st.fieldValidation}>
+                <div className={st.fields}>
+                  <div className={st.field}>
+                    <label htmlFor="cpf">CPF</label>
+                    <span className={st.fieldValidation}>
                     {errors?.cpf ? (errors.cpf.message as string) : null}
                   </span>
-                  <InputMask
-                    mask={"999.999.999-99"}
-                    alwaysShowMask={false}
-                    maskPlaceholder=""
-                    id="cpf"
-                    type="text"
-                    {...register("cpf")}
-                  />
-                </div>
-                <div className={st.field}>
-                  <label htmlFor="dataNascimento">Data de nascimento</label>
-                  <span className={st.fieldValidation}>
+                    <InputMask
+                        mask={"999.999.999-99"}
+                        alwaysShowMask={false}
+                        maskPlaceholder=""
+                        id="cpf"
+                        type="text"
+                        {...register("cpf")}
+                    />
+                  </div>
+                  <div className={st.field}>
+                    <label htmlFor="dataNascimento">Data de nascimento</label>
+                    <span className={st.fieldValidation}>
                     {errors?.dataNascimento
-                      ? (errors.dataNascimento.message as string)
-                      : null}
+                        ? (errors.dataNascimento.message as string)
+                        : null}
                   </span>
-                  <InputMask
-                    mask={"99/99/9999"}
-                    id="dataNascimento"
-                    type="text"
-                    {...register("dataNascimento")}
-                  />
+                    <InputMask
+                        mask={"99/99/9999"}
+                        id="dataNascimento"
+                        type="text"
+                        {...register("dataNascimento")}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className={st.fields}>
-                {/* Senha */}
-                <div className={st.field}>
-                  <label htmlFor="senha">Senha</label>
-                  <span className={st.fieldValidation}>
+                <div className={st.fields}>
+                  {/* Senha */}
+                  <div className={st.field}>
+                    <label htmlFor="senha">Senha</label>
+                    <span className={st.fieldValidation}>
                     {errors?.senha ? (errors.senha.message as string) : null}
                   </span>
-                  <input id="senha" type="password" {...register("senha")} />
-                </div>
-                {/* Confirmar Senha */}
-                <div className={st.field}>
-                  <label htmlFor="confirmPassword">Confirmar senha</label>
-                  <span className={st.fieldValidation}>
+                    <input id="senha" type="password" {...register("senha")} />
+                  </div>
+                  {/* Confirmar Senha */}
+                  <div className={st.field}>
+                    <label htmlFor="confirmPassword">Confirmar senha</label>
+                    <span className={st.fieldValidation}>
                     {errors?.confirmPassword
-                      ? (errors.confirmPassword.message as string)
-                      : null}
+                        ? (errors.confirmPassword.message as string)
+                        : null}
                   </span>
-                  <input
-                    id="confirmPassword"
-                    type="password"
-                    {...register("confirmPassword")}
-                  />
+                    <input
+                        id="confirmPassword"
+                        type="password"
+                        {...register("confirmPassword")}
+                    />
+                  </div>
                 </div>
-              </div>
-              <Button
-                disabled={loading}
-                className="bg-primaryBlue w-[320px] mx-auto block rounded-3xl"
-              >
-                {loading ? "Carregando..." : "Cadastrar"}
-              </Button>
-            </form>
-            {error && <p className={st.error}>{error}</p>}
-            <Link href={"/login"}>
-              Já faz parte? Clique aqui e acesse a sua conta!
-            </Link>
+                <Button
+                    disabled={loading}
+                    className="bg-primaryBlue w-[320px] mx-auto block rounded-3xl"
+                >
+                  {loading ? "Carregando..." : "Cadastrar"}
+                </Button>
+              </form>
+              {error && <p className={st.error}>{error}</p>}
+              <Link href={"/login"}>
+                Já faz parte? Clique aqui e acesse a sua conta!
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
